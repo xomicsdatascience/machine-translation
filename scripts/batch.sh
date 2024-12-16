efc2dfa54354291a0750d28b86367ecf8b17baa1
Cranney <cranneyc@esplhpccompbio-lv01.cm.cluster>
2024-12-16 13:44:22 -0800

    update batch script

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # 
-#SBATCH -p gpu # partition (queue)
-#SBATCH --gpus=1
+#SBATCH -p preemptable,gpu # partition (queue)
+#SBATCH --gpus=a100:1
 #SBATCH -c 1 # number of cores
-#SBATCH --mem 40G # memory pool for all cores
-#SBATCH -t 29-00:00 # time (D-HH:MM)
+#SBATCH --mem 99G # memory pool for all cores
+#SBATCH -t 29-23:00 # time (D-HH:MM)
 #SBATCH --job-name=orgnl-trfrmr
 #SBATCH -o /home/cranneyc/machine-translation/scripts/slurmOutputs/slurm.%j.out # STDOUT
 #SBATCH -e /home/cranneyc/machine-translation/scripts/slurmOutputs/slurm.%j.err # STDERR
