6518f9da52033876e41384952b9b7d5a35154afd
CCranney <11773171+CCranney@users.noreply.github.com>
2025-03-15 13:32:45 -0700

    updated code to account for refactored numeric embedding manager class (for strategy design pattern)

--- src/machine_translation/MachineTranslationModel.py
+++ src/machine_translation/MachineTranslationModel.py
@@ -10,12 +10,12 @@ from attention_smithy.components import Encoder, Decoder, EncoderLayer, DecoderL
 from attention_smithy.numeric_embeddings import (
     SinusoidalPositionEmbedding, LearnedPositionEmbedding,
     RotaryPositionEmbedding, ALiBiPositionEmbedding,
-    NumericEmbeddingManager, NoAddEmbedding, PassthroughEmbedding
+    NumericEmbeddingManager
 )
 from attention_smithy.components import MultiheadAttention, FeedForwardNetwork
 from attention_smithy.attention import StandardAttentionMethod
+from attention_smithy.utils import get_available_gpu_count
 from machine_translation.loss import MaskedLoss, LabelSmoothingLoss
-
 
 class MachineTranslationModel(pl.LightningModule):
     def __init__(self, src_vocab_size: int, tgt_vocab_size: int, tgt_padding_token: int, **kwargs):
@@ -67,49 +67,15 @@ class MachineTranslationModel(pl.LightningModule):
 
         self.save_hyperparameters()
 
-        self.embedding_dimension = self.config['embedding_dimension']
-        self.src_token_embedding = nn.Embedding(src_vocab_size, self.embedding_dimension)
-        self.tgt_token_embedding = nn.Embedding(tgt_vocab_size, self.embedding_dimension)
+        self.src_token_embedding = nn.Embedding(src_vocab_size, self.config['embedding_dimension'])
+        self.tgt_token_embedding = nn.Embedding(tgt_vocab_size, self.config['embedding_dimension'])
 
         self.numeric_embedding_manager = self._create_embedding_manager()
 
-        generic_attention = MultiheadAttention(
-            embedding_dimension=self.embedding_dimension,
-            number_of_heads=self.config['number_of_heads'],
-            attention_method=StandardAttentionMethod(self.config['dropout'])
-        )
-
-        decoder_self_attention = MultiheadAttention(
-            embedding_dimension=self.embedding_dimension,
-            number_of_heads=self.config['number_of_heads'],
-            attention_method=StandardAttentionMethod(self.config['dropout'], is_causal_masking=True)
-        )
-
-        feedforward_network = FeedForwardNetwork(
-            self.embedding_dimension,
-            self.config['feedforward_dimension'],
-            self.config['activation'],
-            self.config['dropout']
-        )
-
-        encoder_layer = EncoderLayer(
-            self.embedding_dimension,
-            generic_attention,
-            feedforward_network,
-            self.config['dropout']
-        )
-        self.encoder = Encoder(encoder_layer, number_of_layers=self.config['num_encoder_layers'])
-
-        decoder_layer = DecoderLayer(
-            self.embedding_dimension,
-            decoder_self_attention,
-            generic_attention,  # Cross attention
-            feedforward_network,
-            self.config['dropout']
-        )
-        self.decoder = Decoder(decoder_layer, number_of_layers=self.config['num_decoder_layers'])
-
-        self.vocab_output_layer = VocabOutputSoftmaxLayer(self.embedding_dimension, tgt_vocab_size)
+        decoder_self_attention, feedforward_network, generic_attention = self._initialize_sublayer_components()
+        self.encoder = self._initialize_encoder(feedforward_network, generic_attention)
+        self.decoder = self._initialize_decoder(decoder_self_attention, feedforward_network, generic_attention)
+        self.vocab_output_layer = VocabOutputSoftmaxLayer(self.config['embedding_dimension'], tgt_vocab_size)
 
         self.loss_method = (
             LabelSmoothingLoss(tgt_padding_token, confidence_probability_score=self.config['label_smoothing'])
@@ -119,52 +85,20 @@ class MachineTranslationModel(pl.LightningModule):
 
         self.scheduler_warmup_steps = self.config['scheduler_warmup_steps']
 
-    def _create_embedding_manager(self):
-        """Create embedding manager with specified embedding types from config."""
-        sinusoidal_position = (
-            SinusoidalPositionEmbedding(self.embedding_dimension)
-            if self.config['use_sinusoidal'] else NoAddEmbedding()
-        )
-
-        learned_position = (
-            LearnedPositionEmbedding(max_sequence_length=3_000, embedding_dimension=self.embedding_dimension)
-            if self.config['use_learned'] else NoAddEmbedding()
-        )
-
-        rotary_position = (
-            RotaryPositionEmbedding(self.embedding_dimension // self.config['number_of_heads'])
-            if self.config['use_rotary'] else PassthroughEmbedding()
-        )
-
-        alibi_position = (
-            ALiBiPositionEmbedding(self.config['number_of_heads'])
-            if self.config['use_alibi'] else NoAddEmbedding()
-        )
-
-        return NumericEmbeddingManager(
-            sinusoidal_position=sinusoidal_position,
-            learned_position=learned_position,
-            rotary_position=rotary_position,
-            alibi_position=alibi_position
-        )
-
     def forward(self, src_tensor, tgt_tensor, src_padding_mask, tgt_padding_mask):
         src_encoded = self.forward_encode(src_tensor, src_padding_mask)
         vocabulary_logits = self.forward_decode(tgt_tensor, src_encoded, tgt_padding_mask, src_padding_mask)
         return vocabulary_logits
 
     def forward_encode(self, src_tensor, src_padding_mask):
-        src_embedding = self.src_token_embedding(src_tensor) * math.sqrt(self.embedding_dimension)
-        position_embedding = self.numeric_embedding_manager.calculate_sinusoidal_and_learned_tokenizations(src_embedding)
+        src_embedding = self.src_token_embedding(src_tensor) * math.sqrt(self.config['embedding_dimension'])
+        position_embedding = self.numeric_embedding_manager.create_positional_or_custom_embedding(token_embedding=src_embedding)
         event_encoded = self.encoder(src=src_embedding + position_embedding, src_padding_mask=src_padding_mask, numeric_embedding_manager=self.numeric_embedding_manager)
         return event_encoded
 
     def forward_decode(self, tgt_tensor, src_encoded, tgt_padding_mask, src_padding_mask):
-        if tgt_tensor.shape[0] != src_encoded.shape[0]:
-            beam_width = tgt_tensor.shape[0] // src_encoded.shape[0]
-            src_encoded = src_encoded.repeat_interleave(beam_width, dim=0)
-            src_padding_mask = src_padding_mask.repeat_interleave(beam_width, dim=0)
-        tgt_embedding = self.tgt_token_embedding(tgt_tensor) * math.sqrt(self.embedding_dimension)
+        src_encoded, src_padding_mask = self._expand_inputs_for_beam_generation_if_applicable(src_encoded, src_padding_mask, tgt_tensor)
+        tgt_embedding = self.tgt_token_embedding(tgt_tensor) * math.sqrt(self.config['embedding_dimension'])
         output = self.decoder(
             tgt=tgt_embedding,
             src=src_encoded,
@@ -192,16 +126,83 @@ class MachineTranslationModel(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = Adam(self.parameters(), lr=1.0, betas=(0.9, 0.98), eps=1e-9)
-        num_gpus = torch.cuda.device_count()
+        num_gpus = get_available_gpu_count()
         effective_warmup_steps = self.scheduler_warmup_steps // num_gpus
 
         def lr_lambda(step):
             step = step + 1
-            lr = self.embedding_dimension ** (-0.5) * min(step ** (-0.5), step * effective_warmup_steps ** (-1.5))
+            lr = self.config['embedding_dimension'] ** (-0.5) * min(step ** (-0.5), step * effective_warmup_steps ** (-1.5))
             return lr
 
         scheduler = LambdaLR(optimizer, lr_lambda=lr_lambda)
         return [optimizer], [{'scheduler': scheduler, 'interval': 'step'}]
+
+
+
+
+
+    def _initialize_decoder(self, decoder_self_attention, feedforward_network, generic_attention):
+        decoder_layer = DecoderLayer(
+            self.config['embedding_dimension'],
+            decoder_self_attention,
+            generic_attention,  # Cross attention
+            feedforward_network,
+            self.config['dropout']
+        )
+        decoder = Decoder(decoder_layer, number_of_layers=self.config['num_decoder_layers'])
+        return decoder
+
+    def _initialize_encoder(self, feedforward_network, generic_attention):
+        encoder_layer = EncoderLayer(
+            self.config['embedding_dimension'],
+            generic_attention,
+            feedforward_network,
+            self.config['dropout']
+        )
+        return Encoder(encoder_layer, number_of_layers=self.config['num_encoder_layers'])
+
+    def _initialize_sublayer_components(self):
+        generic_attention = MultiheadAttention(
+            embedding_dimension=self.config['embedding_dimension'],
+            number_of_heads=self.config['number_of_heads'],
+            attention_method=StandardAttentionMethod(self.config['dropout'])
+        )
+        decoder_self_attention = MultiheadAttention(
+            embedding_dimension=self.config['embedding_dimension'],
+            number_of_heads=self.config['number_of_heads'],
+            attention_method=StandardAttentionMethod(self.config['dropout'], is_causal_masking=True)
+        )
+        feedforward_network = FeedForwardNetwork(
+            self.config['embedding_dimension'],
+            self.config['feedforward_dimension'],
+            self.config['activation'],
+            self.config['dropout']
+        )
+        return decoder_self_attention, feedforward_network, generic_attention
+
+    def _create_embedding_manager(self):
+        embedding_strategies = []
+        if self.config['use_sinusoidal']:
+            embedding_strategies.append(SinusoidalPositionEmbedding(self.config['embedding_dimension']))
+
+        if self.config['use_learned']:
+            embedding_strategies.append(LearnedPositionEmbedding(max_sequence_length=3_000, embedding_dimension=self.config['embedding_dimension']))
+
+        if self.config['use_rotary']:
+            embedding_strategies.append(RotaryPositionEmbedding(self.config['embedding_dimension'] // self.config['number_of_heads']))
+
+        if self.config['use_alibi']:
+            embedding_strategies.append(ALiBiPositionEmbedding(self.config['number_of_heads']))
+
+        return NumericEmbeddingManager(embedding_strategies)
+
+    def _expand_inputs_for_beam_generation_if_applicable(self, src_encoded, src_padding_mask, tgt_tensor):
+        if tgt_tensor.shape[0] != src_encoded.shape[0]:
+            beam_width = tgt_tensor.shape[0] // src_encoded.shape[0]
+            src_encoded = src_encoded.repeat_interleave(beam_width, dim=0)
+            src_padding_mask = src_padding_mask.repeat_interleave(beam_width, dim=0)
+        return src_encoded, src_padding_mask
+
 
 
 class VocabOutputSoftmaxLayer(nn.Module):
